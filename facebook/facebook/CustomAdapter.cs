b154421cc27a9abb1ec0e852b6b4706741500ed8
hindrekhan <hindrek.hannus@tptlive.ee>
2018-10-29 09:52:21 +0200

    abc

---
+++ facebook/facebook/CustomAdapter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace facebook
+{
+    class CustomAdapter : BaseAdapter<Post>
+    {
+        List<Post> items;
+        Activity context;
+
+        public CustomAdapter(Activity context, List<Post> items) : base()
+        {
+            this.context = context;
+            this.items = items;
+        }
+
+        public override Post this[int position]
+        {
+            get { return items[position]; }
+        }
+
+        public override int Count { get { return items.Count; } }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+            if (view == null)
+                view = context.LayoutInflater.Inflate(Resource.Layout.CustomRow, null);
+
+
+            view.FindViewById<TextView>(Resource.Id.name).Text = items[position].Name;
+            view.FindViewById<TextView>(Resource.Id.likes).Text = items[position].Likes.ToString() + " Likes";
+            view.FindViewById<TextView>(Resource.Id.message).Text = items[position].Message;
+            view.FindViewById<TextView>(Resource.Id.comments).Text = items[position].Comments.ToString() + " Comments";
+
+            return view;
+        }
+    }
+}
---
+++ facebook/facebook/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+
+namespace facebook
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            List<Post> posts = new List<Post>();
+            posts.Add(
+                new Post
+                {
+                    Comments = 2,
+                    Likes = 5,
+                    Message = "Hello World!",
+                    Name = "Stefan"
+                });
+
+            posts.Add(
+                new Post
+                {
+                    Comments = 3,
+                    Likes = 235235,
+                    Message = "Hello World!",
+                    Name = "Hindrek"
+                });
+
+            ListView listView = FindViewById<ListView>(Resource.Id.listView1);
+            listView.Adapter = new CustomAdapter(this, posts);
+        }
+    }
+}
